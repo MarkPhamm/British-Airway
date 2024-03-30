98d6fd47a8db34568d6dc4d9dff2e3613c27ccb7
minh pham <minh.b.pham@tcu.edu>
2024-03-30 13:00:15 -0500

    test

--- sql modelling/Test_CTE.sql
+++ sql modelling/Test_CTE.sql
@@ -1,4 +1,5 @@
-SELECT 
-ROW_NUMBER() OVER(ORDER BY date_review) as rating_id,
-ROW_NUMBER() OVER(Partition by customer_name order by customer_name) as customer_Id
-FROM ba_review.dbo.original
+SELECT * FROM ba_review.dbo.fact_rating;
+SELECT * FROM ba_review.dbo.dim_country;
+SELECT * FROM ba_review.dbo.dim_customer;
+SELECT * FROM ba_review.dbo.dim_time;
+SELECT * FROM ba_review.dbo.dim_flight;
