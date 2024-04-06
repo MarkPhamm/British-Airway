5333c510ee43896bab4f81190761bda9c0fda4dd
tranrobin <tran37m@mtholyoke.edu>
2024-04-05 21:24:53 -0400

    fix app

--- review_app.py
+++ review_app.py
@@ -301,41 +301,39 @@ def create_combined_plot(df):
     return fig
 
 def main():
-    # Initialize a session using Amazon S3
-    # aws_access_key_id = st.secrets['aws_access_key_id']
-    # aws_secret_access_key = st.secrets['aws_secret_access_key']
-    # s3_client = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+    #Initialize a session using Amazon S3
+    aws_access_key_id = st.secrets['aws_access_key_id']
+    aws_secret_access_key = st.secrets['aws_secret_access_key']
+    s3_client = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 
-    # # Name of the S3 bucket
-    # bucket_name = 'british-airway'
+    # Name of the S3 bucket
+    bucket_name = 'british-airway'
 
-    # # Function to get the latest CSV file
-    # def get_latest_csv_file(bucket_name):
-    #     csv_files = []
-    #     response = s3_client.list_objects_v2(Bucket=bucket_name)
-    #     for obj in response.get('Contents', []):
-    #         if obj['Key'].endswith('.csv'):
-    #             csv_files.append({'Key': obj['Key'], 'LastModified': obj['LastModified']})
+    # Function to get the latest CSV file
+    def get_latest_csv_file(bucket_name):
+        csv_files = []
+        response = s3_client.list_objects_v2(Bucket=bucket_name)
+        for obj in response.get('Contents', []):
+            if obj['Key'].endswith('.csv'):
+                csv_files.append({'Key': obj['Key'], 'LastModified': obj['LastModified']})
         
-    #     # Sort the files by last modified date in descending order
-    #     latest_csv_file = sorted(csv_files, key=lambda x: x['LastModified'], reverse=True)[0]
-    #     return latest_csv_file['Key']
+        # Sort the files by last modified date in descending order
+        latest_csv_file = sorted(csv_files, key=lambda x: x['LastModified'], reverse=True)[0]
+        return latest_csv_file['Key']
 
-    # # Function to read a CSV file from S3 into a DataFrame
-    # def read_csv_to_df(bucket_name, file_key):
-    #     csv_obj = s3_client.get_object(Bucket=bucket_name, Key=file_key)
-    #     body = csv_obj['Body']
-    #     csv_string = body.read().decode('utf-8')
-    #     df = pd.read_csv(StringIO(csv_string))
-    #     return df
+    # Function to read a CSV file from S3 into a DataFrame
+    def read_csv_to_df(bucket_name, file_key):
+        csv_obj = s3_client.get_object(Bucket=bucket_name, Key=file_key)
+        body = csv_obj['Body']
+        csv_string = body.read().decode('utf-8')
+        df = pd.read_csv(StringIO(csv_string))
+        return df
 
-    # # Get the latest CSV file
-    # latest_csv_file = get_latest_csv_file(bucket_name)
+    # Get the latest CSV file
+    latest_csv_file = get_latest_csv_file(bucket_name)
 
-    # # Read the latest CSV file into a DataFrame
-    # df = read_csv_to_df(bucket_name, latest_csv_file)
-
-    df = pd.read_csv("/Users/robintran/Documents/British-Airway/dataset/clean_data_expand.csv")
+    # Read the latest CSV file into a DataFrame
+    df = read_csv_to_df(bucket_name, latest_csv_file)
 
     # -----------------------------------------------------------
 
