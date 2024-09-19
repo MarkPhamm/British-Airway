0fc485404503aee5a0d4ebaba618a48ce0a4afbc
minh pham <minh.b.pham@tcu.edu>
2024-09-19 16:42:29 -0500

    fea: create scripts and run daily job

---
+++ etl-aws/scripts.py
@@ -0,0 +1,11 @@
+import extract
+import data_cleaning
+import feature_engineering
+
+def main():
+    extract.main()
+    data_cleaning.main()
+    feature_engineering.main()
+
+if __name__ == "__main__":
+    main()
